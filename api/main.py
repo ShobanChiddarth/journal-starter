4fa63505b7376ed59b75a083ef30051f6805fa51
Shoban Chiddarth <messageshoban@gmail.com>
2026-02-13 22:59:45 +0530

    Removed TODO comment

--- api/main.py
+++ api/main.py
@@ -13,14 +13,6 @@ logging.basicConfig(level=logging.INFO)
 consoleHandler = logging.StreamHandler(sys.stdout)
 logger = logging.getLogger(__name__)
 
-# TODO: Setup basic console logging
-# Hint: Use logging.basicConfig() with level=logging.INFO
-# Steps:
-# 1. Configure logging with basicConfig()
-# 2. Set level to logging.INFO
-# 3. Add console handler
-# 4. Test by adding a log message when the app starts
-
 app = FastAPI(title="Journal API", description="A simple journal API for tracking daily work, struggles, and intentions")
 app.include_router(journal_router)
 
