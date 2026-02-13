cd9e855552c7eea104ac46e922940f8ae2acc709
Shoban Chiddarth <messageshoban@gmail.com>
2026-02-13 21:29:10 +0530

    Implemented logging on api/main.py

--- api/main.py
+++ api/main.py
@@ -7,6 +7,11 @@ from fastapi import FastAPI
 
 from api.routers.journal_router import router as journal_router
 
+import logging
+import sys
+logging.basicConfig(level=logging.INFO)
+consoleHandler = logging.StreamHandler(sys.stdout)
+logger = logging.getLogger(__name__)
 
 # TODO: Setup basic console logging
 # Hint: Use logging.basicConfig() with level=logging.INFO
@@ -18,3 +23,7 @@ from api.routers.journal_router import router as journal_router
 
 app = FastAPI(title="Journal API", description="A simple journal API for tracking daily work, struggles, and intentions")
 app.include_router(journal_router)
+
+@app.on_event("startup")
+async def startup_log():
+    logger.info("App started")
